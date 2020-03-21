ccc7c4368bff19e509e07f36abae6f746fd14ee2
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2020-03-21 04:09:54 -0400

    look at this

---
+++ src/ExpertComputerPlayer.java
@@ -0,0 +1,19 @@
+public class ExpertComputerPlayer extends ComputerPlayer
+{
+    public ExpertComputerPlayer(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public boolean attack(Player enemy, Location locx)
+    {
+        return super.attack(enemy, locx);
+    }
+
+    @Override
+    public void populateShips()
+    {
+        super.populateShips();
+    }
+}
